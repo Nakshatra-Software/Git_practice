24fc740666f610abd75475d9a5159cb3feac4ac7
Saravana <lg_saravana2005@yahoo.co.in>
2019-10-22 13:05:18 +0530

    last commit

---
+++ oops.java
@@ -0,0 +1,31 @@
+// lecture 22
+// OOP concepts.
+public class Main {
+
+    public static void main(String[] args) {
+
+        class MyClass {
+
+            String name;
+            int age;
+        }
+
+        MyClass object1 = new MyClass();
+        object1.name = "Ollie";
+        object1.age = 18;
+
+        MyClass object2 = new MyClass();
+        object2.name = "Mark";
+        object2.age = 21;
+
+        System.out.println(object1.name + " is " + object1.age);
+
+        System.out.println(object2.name + " is " + object2.age);
+		System.out.println("from 2020 to master");
+                system.out.println("final release code ");
+                //system.out.println("by mistake updated bug fix code master instead of final release ");		
+		system.out.println(" updated with latest fix on oops.java project"
+                
+
+    }
+}
